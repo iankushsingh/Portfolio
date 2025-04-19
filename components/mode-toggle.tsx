8dbc1835ac9f3d652eaa9a9632eb949d1a230b62
Ankush Singh Bhadauriya <connectankush.07@gmail.com>
2025-04-19 14:19:54 +0530

    Update mode-toggle.tsx

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -1,8 +1,9 @@
 "use client"
+
+import { useEffect, useState } from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 import { Button } from "@/components/ui/button"
-import { useEffect, useState } from "react"
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme()
